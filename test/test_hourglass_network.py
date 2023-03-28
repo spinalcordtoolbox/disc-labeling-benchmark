82e1f1149f2ca56647ffff706a96eb0184ee5fcc
Nathan Molinier <nathan.molinier@gmail.com>
2023-03-28 17:59:32 -0400

    Add step to project coordinates on centerline + add comments

--- test/test_hourglass_network.py
+++ test/test_hourglass_network.py
@@ -15,7 +15,7 @@ sys.path.append(parent_dir)
 from models.hourglass import hg
 from models.atthourglass import atthg
 from utils.train_utils import image_Dataset
-from utils.test_utils import CONTRAST, extract_skeleton, best_disc_association, visualize_discs, swap_y_origin, coord2list 
+from utils.test_utils import CONTRAST, extract_skeleton, best_disc_association, swap_y_origin, coord2list, project_on_spinal_cord 
 
 #---------------------------Test Hourglass Network----------------------------
 def test_hourglass(args):
@@ -23,6 +23,7 @@ def test_hourglass(args):
     contrast = CONTRAST[args.contrast]
     txt_file = args.out_txt_file
     ndiscs = args.ndiscs
+    origin_data = args.sct_datapath
     
     print('load images')               
     with open(args.hg_datapath, 'rb') as file_pi:
@@ -67,12 +68,13 @@ def test_hourglass(args):
         file_lines = f.readlines()
         split_lines = [line.split(' ') for line in file_lines]
     
-    # Get the visualization results of the test set
+    # Extract discs coordinates from the test set
     for i, (input, target, vis, gt_coord, subject_name) in enumerate(MRI_test_loader): # subject_name
         input, target = input.to(device), target.to(device, non_blocking=True)
         output = model(input) 
         output = output[-1]
         original_img = np.rot90(full[0][i])  # rotate original input image to normal position 
+        subject_name = subject_name[0]
         
         prediction = extract_skeleton(input, output, target, norm_mean_skeleton, ndiscs, Flag_save = True)
         prediction = np.sum(prediction[0], axis = 0)
@@ -80,16 +82,27 @@ def test_hourglass(args):
         num_labels, labels_im, states, centers = cv2.connectedComponentsWithStats(np.uint8(np.where(prediction>0, 255, 0)))
         
         
-        # Write the predicted and ground truth coordinates inside the discs_coords txt file
+        # Extract prediction and ground truth
         pred = centers[1:] #0 for background
         pred = swap_y_origin(coords=pred, img_shape=original_img.shape)  # Move y origin to the bottom of the image
-        pred = coord2list(pred[~pred[:, 1].argsort()])  # Sorting predictions according to first coordinate and swap to coords convention [x, y] --> [lines(y), columns(x)]
         gt_coord = np.array(torch.tensor(gt_coord).tolist())
-        gt_coord = np.transpose(np.array([gt_coord[:,2],gt_coord[:,1],gt_coord[:,-1]])) # Using same format as prediction + discs label
-        subject_index = np.where((np.array(split_lines)[:,0] == subject_name[0]) & (np.array(split_lines)[:,1] == contrast))  
-        start_index = subject_index[0][0]  # Getting the first line in the txt file
         
+        # Project coordinate onto the spinal cord centerline 
+        seg_path = os.path.join(origin_data, subject_name, f'{subject_name}_{contrast}_seg.nii.gz' )
+        pred = project_on_spinal_cord(coords=pred, seg_path=seg_path, disc_num=False, proj_2d=True)
+        gt_coord = project_on_spinal_cord(coords=gt_coord, seg_path=seg_path, disc_num=True, proj_2d=False)
+        
+        # Rearrange coordinates
+        pred = coord2list(pred[~pred[:, 1].argsort()])  # Sorting predictions according to first coordinate and swap to coords convention [x, y] --> [lines(y), columns(x)]
+        gt_coord = np.transpose(np.array([gt_coord[:,2],gt_coord[:,1],gt_coord[:,-1]])) # Using same format as prediction + discs label
+
+        # Get best association between pred and gt
+        # TODO - Extract discs numbers from prediction to avoid this step
         pred, gt = best_disc_association(pred=pred, gt=gt_coord)
+        
+        # Write coordinates in txt file
+        subject_index = np.where((np.array(split_lines)[:,0] == subject_name) & (np.array(split_lines)[:,1] == contrast))  
+        start_index = subject_index[0][0]  # Getting the first line in the txt file
         for i in range(len(pred)):
             pred_coord = pred[i] if pred[i]!=0 else 'Fail'
             gt_coord = gt[i] if gt[i]!=0 else 'None'
