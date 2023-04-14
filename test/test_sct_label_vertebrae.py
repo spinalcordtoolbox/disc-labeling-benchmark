55a4d626256139afe63370198fbff94f73a6123c
Nathan Molinier <nathan.molinier@gmail.com>
2023-04-14 12:42:49 -0400

    Remove commented code

--- test/test_sct_label_vertebrae.py
+++ test/test_sct_label_vertebrae.py
@@ -66,38 +66,11 @@ def test_sct_label_vertebrae(args):
                     discs_coords = 'Fail'
 
             subject_name = dir_name
-            # Edit txt_file --> line = subject_name contrast disc_num ground_truth_coord sct_label_vertebrae_coord hourglass_coord
-            # if (subject_name + '_' + contrast) not in processed_subjects_with_contrast:
-            #     if discs_coords == 'Fail':  # SCT method error
-            #         lines = [subject_name + ' ' + contrast + ' ' + str(disc_num + 1) + ' ' + 'None' + ' ' + 'Fail' + ' ' + 'None' + '\n' for disc_num in range(11)] # To reorder the discs
-            #     else:
-            #         lines = [subject_name + ' ' + contrast + ' ' + str(disc_num + 1) + ' ' + 'None' + ' ' + 'None' + ' ' + 'None' + '\n' for disc_num in range(11)] # To reorder the discs
-            #         last_referred_disc = 0
-            #         for coord in discs_coords:
-            #             coord_list = list(coord)
-            #             disc_num = int(coord_list[-1])
-            #             coord_2d = '[' + str(coord_list[2]) + ',' + str(coord_list[1]) + ']'#  2D comparison of the models
-            #             if disc_num > 11:
-            #                 print('More than 11 discs are visible')
-            #                 print('Disc number', disc_num)
-            #                 if disc_num == last_referred_disc + 1:  # Check if all the previous discs were also implemented
-            #                     lines.append(subject_name + ' ' + contrast + ' ' + str(disc_num) + ' ' + 'None' + ' ' + coord_2d + ' ' + 'None' + '\n')
-            #                     last_referred_disc = disc_num
-            #                 else:
-            #                     for i in range(disc_num - last_referred_disc - 1):
-            #                         lines.append(subject_name + ' ' + contrast + ' ' + str(last_referred_disc + 1 + i) + ' ' + 'None' + ' ' + 'None' + ' ' + 'None' + '\n')
-            #                     lines.append(subject_name + ' ' + contrast + ' ' + str(disc_num) + ' ' + 'None' + ' ' + coord_2d + ' ' + 'None' + '\n')
-            #                     last_referred_disc = disc_num
-            #             else:
-            #                 lines[disc_num-1] = subject_name + ' ' + contrast + ' ' + str(disc_num) + ' ' + 'None' + ' ' + coord_2d + ' ' + 'None' + '\n'
-            #                 last_referred_disc = disc_num
-            #     with open(txt_file,"a") as f:
-            #         f.writelines(lines)
-                    
             subject_index = np.where((np.array(split_lines)[:,0] == subject_name) & (np.array(split_lines)[:,1] == contrast))  
             start_index = subject_index[0][0]  # Getting the first line for the subject in the txt file
             last_index = subject_index[0][-1]  # Getting the last line for the subject in the txt file
             max_ref_disc = int(split_lines[last_index][2])  # Getting the last refferenced disc num
+            # Edit txt_file --> line = subject_name contrast disc_num ground_truth_coord sct_label_vertebrae_coord hourglass_coord    
             if discs_coords == 'Fail':  # SCT method error
                 for i in range(len(subject_index[0])):
                     split_lines[start_index + i][4] = 'Fail'
