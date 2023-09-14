f0e6cd9ccf03815687ac3fe8c7d2d1e335627b2d
Nathan Molinier <nathan.molinier@gmail.com>
2023-09-14 15:15:55 -0400

    Create default script for nnunet

---
+++ src/bcm/methods/test_nnunet_network.py
@@ -0,0 +1,96 @@
+import os
+import json
+import argparse
+import numpy as np
+import subprocess
+
+from bcm.utils.utils import SCT_CONTRAST, edit_subject_lines_txt_file, fetch_img_and_seg_paths, fetch_subject_and_session, fetch_contrast
+from bcm.utils.image import Image
+
+
+#---------------------------Test Sct Label Vertebrae--------------------------
+def test_nnunet(args):
+    '''
+    Run nnunet inference for vertebral labeling and append the discs coordinates to a txt file
+    '''
+    txt_file = args.out_txt_file
+    seg_suffix = args.suffix_seg
+    
+    # Read json file and create a dictionary
+    with open(args.config_data, "r") as file:
+        config_data = json.load(file)
+
+    # Get image and segmentation paths
+    img_paths, seg_paths = fetch_img_and_seg_paths(path_list=config_data['TESTING'], 
+                                                   path_type=config_data['TYPE'], 
+                                                   seg_suffix=seg_suffix, 
+                                                   derivatives_path='derivatives/labels')
+    
+    # Extract txt file lines
+    with open(txt_file,"r") as f:
+        file_lines = f.readlines()
+        split_lines = [line.split(' ') for line in file_lines]
+    
+    print('Processing with sct_label_vertebrae')
+    for img_path, seg_path in zip(img_paths, seg_paths):
+
+        # Fetch contrast, subject, session and echo
+        subjectID, sessionID, _, _, echoID, acq = fetch_subject_and_session(img_path)
+        sub_name = subjectID
+        if acq:
+            sub_name += f'_{acq}'
+        if sessionID:
+            sub_name += f'_{sessionID}'
+        if echoID:
+            sub_name += f'_{echoID}'
+        contrast = fetch_contrast(img_path)
+
+        # Look for segmentation path
+        add_subject = False
+        back_up_seg_path = os.path.join(args.seg_folder, 'derivatives-seg', seg_path.split('derivatives/')[-1])
+        if os.path.exists(seg_path) and Image(seg_path).change_orientation('RSP').data.shape==Image(img_path).change_orientation('RSP').data.shape:  # Check if seg_shape == img_shape or create new seg
+            add_subject = True
+        elif args.create_seg and os.path.exists(back_up_seg_path) and Image(back_up_seg_path).change_orientation('RSP').data.shape==Image(img_path).change_orientation('RSP').data.shape:
+            seg_path = back_up_seg_path
+            add_subject = True
+        
+        if add_subject: # A segmentation is available for projection
+            
+            
+            # Edit coordinates in txt file
+            # line = subject_name contrast disc_num gt_coords sct_discs_coords hourglass_coords spinenet_coords
+            split_lines = edit_subject_lines_txt_file(coords=discs_coords, txt_lines=split_lines, subject_name=sub_name, contrast=contrast, method_name='nnunet_coords')
+        else:
+            print(f'No segmentation is available for {img_path}')
+
+    for num in range(len(split_lines)):
+        split_lines[num] = ' '.join(split_lines[num])
+        
+    with open(txt_file,"w") as f:
+        f.writelines(split_lines)
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Add nnunet coordinates to text file')
+
+    ## Parameters
+    # All mandatory parameters                         
+    parser.add_argument('--config-data', type=str, metavar='<folder>', required=True,
+                        help='Config JSON file where every label/image used for TESTING has its path specified ~/<your_path>/config_data.json (Required)')
+    parser.add_argument('-txt', '--out-txt-file', required=True,
+                        type=str, metavar='N',help='Generated txt file path (e.g. "results/files/(CONTRAST)_discs_coords.txt") (Required)')                             
+    
+    # All methods
+    parser.add_argument('--suffix-seg', type=str, default='_seg-manual',
+                        help='Specify segmentation label suffix example: sub-296085_T2w(SEG_SUFFIX).nii.gz (default= "_seg")')
+    parser.add_argument('--seg-folder', type=str, default='results',
+                        help='Path to segmentation folder where non existing segmentations will be created. ' 
+                        'These segmentations will be used to project labels onto the spinalcord (default="results")')
+    parser.add_argument('--create-seg', type=bool, default=False,
+                        help='To perform this benchmark, SC segmentation are needed for projection to compare the methods. '
+                        'Set this variable to True to create segmentation using sct_deepseg_sc when not available')
+    
+    # Run sct_label_vertebrae on input data
+    test_nnunet(parser.parse_args())
+
+    print('nnunet coordinates have been added')
